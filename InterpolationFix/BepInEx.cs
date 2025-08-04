770d7102c6b480ea6de83ff282d7c371350106fa
jbeast291 <jacoby29181@gmail.com>
2025-08-03 17:34:05 -0700

    fmu ipdate. FML

--- InterpolationFix/BepInEx.cs
+++ InterpolationFix/BepInEx.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Nautilus.Utility.ModMessages;
 
 namespace InterpolationFix;
 
@@ -8,7 +9,7 @@ namespace InterpolationFix;
 public class BepInEx : BaseUnityPlugin
 {
     private const string myGUID = "Jbeast291.InterpolationFix";
-    private const string pluginName = "Interpolaion Fix";
+    private const string pluginName = "Interpolation Fix";
     private const string pluginVersion = "1.0.1";
 
     private static readonly Harmony harmony = new Harmony(myGUID);
@@ -16,6 +17,7 @@ public class BepInEx : BaseUnityPlugin
     internal static new ManualLogSource Logger;
     public void Awake()
     {
+        ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/jbeast291/Subnautica-mod/refs/heads/main/InterpolationFix/FMU.json");
         Logger = base.Logger;
         Logger.LogInfo(pluginName + " " + pluginVersion + " " + "has been loaded!");
         Logger.LogInfo("Patching...");
