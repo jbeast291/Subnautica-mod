e345daa87f8432924cec0d4ee670f34d9aac83c2
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-01-12 18:17:20 -0800

    update interpolation fix build system and transpiler patches

--- InterpolationFix/Patches/FreezeRigidbodyWhenFarPatch.cs
+++ InterpolationFix/Patches/FreezeRigidbodyWhenFarPatch.cs
@@ -1,44 +1,35 @@
 ﻿using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using UnityEngine;
+using UnityEngine.TextCore;
 
-namespace InterpolationFix.Patches
-{
-    /// <summary>
-    /// Transpiler patch for FreezeRigidbodyWhenFar to replace calls to UWE.Utils.SetIsKinematicAndUpdateInterpolation(...) with just setting
-    /// the rigidbody to kinematic. This ensures it doesnt touch interpolation (it doesn't even need to, to function).
-    /// </summary>
-    [HarmonyPatch(typeof(FreezeRigidbodyWhenFar))]
-    [HarmonyPatch("FixedUpdate")]
-    internal class FreezeRigidbodyWhenFarPatch
+namespace InterpolationFix.Patches;
+
+/// <summary>
+/// Transpiler patch for FreezeRigidbodyWhenFar to replace calls to UWE.Utils.SetIsKinematicAndUpdateInterpolation(...) with just setting
+/// the rigidbody to kinematic. This ensures it doesn't touch interpolation (it doesn't even need to, to function).
+/// </summary>
+[HarmonyPatch(typeof(FreezeRigidbodyWhenFar), nameof(FreezeRigidbodyWhenFar.FixedUpdate))]
+internal class FreezeRigidbodyWhenFarPatch
+{ 
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var codes = new List<CodeInstruction>(instructions);
+        CodeMatcher matcher = new CodeMatcher(instructions);
 
-            var targetMethod = AccessTools.Method(
-                typeof(UWE.Utils),//there is abuguity so must be very specific :/
-                "SetIsKinematicAndUpdateInterpolation",
-                new Type[] { typeof(Rigidbody), typeof(bool), typeof(bool) }
-            );
+        
+        ReplaceSetIsKinematicAndUpdateInterpolationCalls(matcher);
+        ReplaceSetIsKinematicAndUpdateInterpolationCalls(matcher);
+        
+        return matcher.InstructionEnumeration();
+    }
 
-            var isKinematicSetter = AccessTools.PropertySetter(typeof(Rigidbody), "isKinematic");
-
-            for (int i = 1; i < codes.Count; i++)
-            {
-                if (//find UWE.Utils.SetIsKinematicAndUpdateInterpolation(...) call
-                    codes[i].Calls(targetMethod)
-                )
-                {
-                    //rigidbody ref and true opcode already there, so use them
-                    codes[i - 1] = new CodeInstruction(OpCodes.Callvirt, isKinematicSetter);//set rigidbody to kinematic
-                    codes.RemoveAt(i);//dont need the fourth instruction
-                }
-            }
-            BepInEx.Logger.LogInfo("Finished Transpiler for FreezeRigidbodyWhenFar.FixedUpdate...");
-            return codes;
-        } 
+    public static void ReplaceSetIsKinematicAndUpdateInterpolationCalls(CodeMatcher matcher)
+    {
+        matcher
+            .MatchForward(true, new CodeMatch(ci => ci.opcode == OpCodes.Ldloc_0))//find where it loads rigidbody
+            .Advance(2)//hop past ldc.i4.1 and onto ldc.i4.0
+            .RemoveInstruction()//remove ldc.i4.0;
+            .SetAndAdvance(OpCodes.Call, AccessTools.PropertySetter(typeof(Rigidbody), nameof(Rigidbody.isKinematic)));//replace old call with just a set
     }
 }
---
+++ InterpolationFix/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Utility.ModMessages;
+
+namespace InterpolationFix;
+
+[BepInPlugin(MyGuid, PluginName, PluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string MyGuid = "Jbeast291.InterpolationFix";
+    private const string PluginName = "Interpolation Fix";
+    private const string PluginVersion = "1.0.1";
+
+    private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+    internal static new ManualLogSource Logger;
+    public void Awake()
+    {
+        Logger = base.Logger;
+        
+        ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/jbeast291/Subnautica-mod/refs/heads/main/InterpolationFix/FMU.json");
+        
+        Logger.LogInfo(PluginName + " " + PluginVersion + " " + "has been loaded!");
+        Logger.LogInfo("Patching...");
+        Harmony.PatchAll();
+        Logger.LogInfo("Patched!");
+    }
+}
