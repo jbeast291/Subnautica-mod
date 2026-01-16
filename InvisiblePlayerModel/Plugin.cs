67931c2088a8bf7e1f83ec41a04c64d4dfd6c720
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-01-15 19:56:34 -0800

    FMU update

--- InvisiblePlayerModel/Plugin.cs
+++ InvisiblePlayerModel/Plugin.cs
@@ -12,7 +12,7 @@ public class Plugin : BaseUnityPlugin
 {
     private const string MY_GUID = "Jbeast291.InvisiblePlayerModel";
     private const string PLUGIN_NAME = "Invisible Player Model";
-    private const string PLUGIN_VERSION = "0.0.1";
+    private const string PLUGIN_VERSION = "1.0.0";
     
     public static InvisiblePlayerModelConfig config { get; } = OptionsPanelHandler.RegisterModOptions<InvisiblePlayerModelConfig>();
     
@@ -23,14 +23,12 @@ public class Plugin : BaseUnityPlugin
     {
         Logger = base.Logger;
         
-        ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/jbeast291/Subnautica-mod/refs/heads/main/InterpolationFix/FMU.json");
-        
-        Harmony.PatchAll();
+        ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/jbeast291/Subnautica-mod/refs/heads/main/InvisiblePlayerModel/FMU.json");
         Logger.LogInfo($"Plugin {MY_GUID} ({PLUGIN_VERSION}) is loaded!");
         
-        WaitScreenHandler.RegisterLateLoadTask(PLUGIN_NAME, AttachInterpolationManager, "SceneLoadListener");
+        WaitScreenHandler.RegisterLateLoadTask(PLUGIN_NAME, InitialPlayerModelUpdate, "SceneLoadListener");
     }
-    private void AttachInterpolationManager(WaitScreenHandler.WaitScreenTask waitScreenTask)
+    private void InitialPlayerModelUpdate(WaitScreenHandler.WaitScreenTask waitScreenTask)
     {
         waitScreenTask.Status = "Modifying Player Model...";
         Logger.LogInfo("Modifying Player Model...");
