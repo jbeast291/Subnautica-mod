f0c79786bb5483dc3f1a6151bd47d303d1a400d2
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-01-15 20:17:51 -0800

    Interpolation fix FMU push

--- InterpolationFix/Plugin.cs
+++ InterpolationFix/Plugin.cs
@@ -7,15 +7,15 @@ using Nautilus.Utility.ModMessages;
 
 namespace InterpolationFix;
 
-[BepInPlugin(MyGuid, PluginName, PluginVersion)]
+[BepInPlugin(MY_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
 [BepInDependency("com.snmodding.nautilus")]
 public class Plugin : BaseUnityPlugin
 {
-    private const string MyGuid = "Jbeast291.InterpolationFix";
-    private const string PluginName = "Interpolation Fix";
-    private const string PluginVersion = "1.0.1";
+    private const string MY_GUID = "Jbeast291.InterpolationFix";
+    private const string PLUGIN_NAME = "Interpolation Fix";
+    private const string PLUGIN_VERSION = "1.1.0";
 
-    private static readonly Harmony Harmony = new Harmony(MyGuid);
+    private static readonly Harmony Harmony = new Harmony(MY_GUID);
 
     internal new static ManualLogSource Logger;
     
@@ -25,11 +25,11 @@ public class Plugin : BaseUnityPlugin
         
         ModMessageSystem.SendGlobal("FindMyUpdates", "https://raw.githubusercontent.com/jbeast291/Subnautica-mod/refs/heads/main/InterpolationFix/FMU.json");
         
-        Logger.LogInfo(PluginName + " " + PluginVersion + " " + "has been loaded!");
+        Logger.LogInfo(PLUGIN_NAME + " " + PLUGIN_VERSION + " " + "has been loaded!");
         Harmony.PatchAll();
         Logger.LogInfo("Patched Methods!");
         
-        WaitScreenHandler.RegisterLateLoadTask(PluginName, AttachInterpolationManager, "SceneLoadListener");
+        WaitScreenHandler.RegisterLateLoadTask(PLUGIN_NAME, AttachInterpolationManager, "SceneLoadListener");
     }
 
     private void AttachInterpolationManager(WaitScreenHandler.WaitScreenTask waitScreenTask)
